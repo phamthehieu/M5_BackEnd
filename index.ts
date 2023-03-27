e3fa9749cec6141fdc8e5ac3accde53b32fa25ee
phamthehieu <111071418+phamthehieu@users.noreply.github.com>
2023-03-27 16:23:58 +0700

    2

--- index.ts
+++ index.ts
@@ -1,37 +1,16 @@
-import {AppDataSource} from "./src/data-soure";
-import express from 'express';
-import bodyParser from "body-parser";
-import cors from "cors"
-import {router} from "./src/router/router";
+import express, { Request, Response } from 'express'
 
-const http = require("http");
+const app = express()
+const port = process.env.PORT || 8080
 
-const app = express();
-const server = http.createServer(app);
-AppDataSource.initialize().then(() => {
-    console.log(" Connect Server! ")
+app.get('/', (_req: Request, res: Response) => {
+    return res.send('Express Typescript on Vercel')
 })
-const socketIo = require('socket.io')(server, {
-    cors: {
-        origin: '*'
-    }
+
+app.get('/ping', (_req: Request, res: Response) => {
+    return res.send('pong 🏓')
 })
-socketIo.on("connection", (socket) => {
-    console.log("New client connected" + socket.id);
 
-    socket.on("sendDataClient", function (data) {
-        socketIo.emit("sendDataServer", {data});
-    })
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
-});
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('./public'));
-app.use('', router);
-app.listen(3000, () => {
-    console.log(' Connect Server! ')
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`)
 })
